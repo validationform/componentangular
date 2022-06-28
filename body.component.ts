6f2608349826c4c6bfe4bd3a0e9e58d2b968c1de
Rashmi Bora <107795961+validationform@users.noreply.github.com>
2022-06-28 10:03:36 +0530

    Add files via upload

---
+++ body.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.scss']
+})
+export class BodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  url1="./assets/i.jpg";
+  url2="./assets/fmf_3975-scaled.jpg";
+  url3="./assets/f_3973-scaled.jpg";
+}
